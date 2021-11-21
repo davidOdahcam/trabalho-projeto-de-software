c010c6111b38c9b114878e95e30a38ff8616bb72
David dos Santos Machado <david.desenvolvedor@gmail.com>
2021-11-21 12:23:35 -0300

    Atualizando reconhecimento de rotas no menu lateral

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -10,78 +10,78 @@
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.aircraft.index') }}" class="nav-link {{ activeMenu('aeronaves*') }}">
+                <a href="{{ route('admin.aircraft.index') }}" class="nav-link {{ activeMenu('admin/aeronaves*') }}">
                     <i class="fas fa-plane nav-icon"></i>
                     <p>Aeronaves</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.airline.index') }}" class="nav-link {{ activeMenu('companhias-aereas*') }}">
+                <a href="{{ route('admin.airline.index') }}" class="nav-link {{ activeMenu('admin/companhias-aereas*') }}">
                     <i class="far fa-building nav-icon"></i>
                     <p>Companhias aéreas</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.airport.index') }}" class="nav-link {{ activeMenu('aeroportos*') }}">
+                <a href="{{ route('admin.airport.index') }}" class="nav-link {{ activeMenu('admin/aeroportos*') }}">
                     <i class="fas fa-plane-arrival nav-icon"></i>
                     <p>Aeroportos</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.booking.index') }}" class="nav-link {{ activeMenu('reservas*') }}">
+                <a href="{{ route('admin.booking.index') }}" class="nav-link {{ activeMenu('admin/reservas*') }}">
                     <i class="fas fa-suitcase-rolling nav-icon"></i>
                     <p>Reservas</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.country.index') }}" class="nav-link {{ activeMenu('paises*') }}">
+                <a href="{{ route('admin.country.index') }}" class="nav-link {{ activeMenu('admin/paises*') }}">
                     <i class="fas fa-globe-americas nav-icon"></i>
                     <p>Países</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.equipment.index') }}" class="nav-link {{ activeMenu('equipamentos*') }}">
+                <a href="{{ route('admin.equipment.index') }}" class="nav-link {{ activeMenu('admin/equipamentos*') }}">
                     <i class="fas fa-tools nav-icon"></i>
                     <p>Equipamentos</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.flight.index') }}" class="nav-link {{ activeMenu('voos*') }}">
+                <a href="{{ route('admin.flight.index') }}" class="nav-link {{ activeMenu('admin/voos*') }}">
                     <i class="fas fa-plane-departure nav-icon"></i>
                     <p>Voos</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.passenger.index') }}" class="nav-link {{ activeMenu('passageiros*') }}">
+                <a href="{{ route('admin.passenger.index') }}" class="nav-link {{ activeMenu('admin/passageiros*') }}">
                     <i class="fas fa-user-friends nav-icon"></i>
                     <p>Passageiros</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.route.index') }}" class="nav-link {{ activeMenu('rotas-de-voo*') }}">
+                <a href="{{ route('admin.route.index') }}" class="nav-link {{ activeMenu('admin/rotas-de-voo*') }}">
                     <i class="fas fa-map-marked-alt nav-icon"></i>
                     <p>Rotas de voo</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.state.index') }}" class="nav-link {{ activeMenu('unidades-federais*') }}">
+                <a href="{{ route('admin.state.index') }}" class="nav-link {{ activeMenu('admin/unidades-federais*') }}">
                     <i class="fas fa-map-marker-alt nav-icon"></i>
                     <p>Unidades federais</p>
                 </a>
             </li>
 
             {{-- RELATÓRIOS --}}
-            <li class="nav-item {{ activeMenu('relatorios*', true) }}">
-                <a href="#" class="nav-link {{ activeMenu('relatorios*') }}">
+            <li class="nav-item {{ activeMenu('admin/relatorios*', true) }}">
+                <a href="#" class="nav-link {{ activeMenu('admin/relatorios*') }}">
                     <i class="fas fa-folder nav-icon"></i>
                     <p>
                         Relatórios
@@ -91,14 +91,14 @@
 
                 <ul class="nav nav-treeview">
                     <li class="nav-item">
-                        <a href="{{ route('admin.report.airline_by_country') }}" class="nav-link {{ activeMenu('relatorios/companhias-por-paises*') }}">
+                        <a href="{{ route('admin.report.airline_by_country') }}" class="nav-link {{ activeMenu('admin/relatorios/companhias-por-paises*') }}">
                             <i class="fas fa-file nav-icon"></i>
                             <p>Companhias por países</p>
                         </a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.report.bookings_passengers_300') }}" class="nav-link {{ activeMenu('relatorios/reservas-de-passageiros*') }}">
+                        <a href="{{ route('admin.report.bookings_passengers_300') }}" class="nav-link {{ activeMenu('admin/relatorios/reservas-de-passageiros*') }}">
                             <i class="fas fa-file nav-icon"></i>
                             <p>Reservas de passageiros</p>
                         </a>
@@ -112,42 +112,42 @@
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.report.equipments') }}" class="nav-link {{ activeMenu('relatorios/equipamentos*') }}">
+                        <a href="{{ route('admin.report.equipments') }}" class="nav-link {{ activeMenu('admin/relatorios/equipamentos*') }}">
                             <i class="fas fa-file nav-icon"></i>
                             <p>Equipamentos</p>
                         </a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.report.passengers') }}" class="nav-link {{ activeMenu('relatorios/passageiros*') }}">
+                        <a href="{{ route('admin.report.passengers') }}" class="nav-link {{ activeMenu('admin/relatorios/passageiros*') }}">
                             <i class="fas fa-file nav-icon"></i>
                             <p>Passageiros</p>
                         </a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.report.flights_city') }}" class="nav-link {{ activeMenu('relatorios/voos-por-cidade*') }}">
+                        <a href="{{ route('admin.report.flights_city') }}" class="nav-link {{ activeMenu('admin/relatorios/voos-por-cidade*') }}">
                             <i class="fas fa-file nav-icon"></i>
                             <p>Voos por cidade</p>
                         </a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.report.carrying_capacity') }}" class="nav-link {{ activeMenu('relatorios/capacidade-de-transporte*') }}">
+                        <a href="{{ route('admin.report.carrying_capacity') }}" class="nav-link {{ activeMenu('admin/relatorios/capacidade-de-transporte*') }}">
                             <i class="fas fa-file nav-icon"></i>
                             <p>Capacidade de transporte</p>
                         </a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.report.average_age') }}" class="nav-link {{ activeMenu('relatorios/media-de-idade*') }}">
+                        <a href="{{ route('admin.report.average_age') }}" class="nav-link {{ activeMenu('admin/relatorios/media-de-idade*') }}">
                             <i class="fas fa-file nav-icon"></i>
                             <p>Média de idade</p>
                         </a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.report.flights_by_route') }}" class="nav-link {{ activeMenu('relatorios/voos-por-rota*') }}">
+                        <a href="{{ route('admin.report.flights_by_route') }}" class="nav-link {{ activeMenu('admin/relatorios/voos-por-rota*') }}">
                             <i class="fas fa-file nav-icon"></i>
                             <p>Voos por rota</p>
                         </a>
