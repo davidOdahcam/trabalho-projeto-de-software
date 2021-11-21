cf39fe87779cc61a69c88e930e7f59b07a6eb89d
David dos Santos Machado <david.desenvolvedor@gmail.com>
2021-11-21 12:31:27 -0300

    Adicionando função que adiciona uma mensagem flash à sessão

--- app/helpers/general.php
+++ app/helpers/general.php
@@ -9,3 +9,15 @@ if (!function_exists('activeMenu')) {
         }
     }
 }
+
+if (!function_exists('sessionFlash')) {
+    function sessionFlash(string $key, $value = null) {
+        dd(app());
+        try {
+            $session = app('session');
+            $session->flash($key, $value);
+        } catch (\Throwable $th) {
+            // Tratar log de erros
+        }
+    }
+}
